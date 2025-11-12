1f602fb01d8b76c86313c56d7941805982ae75b3
BahaObaid01 <bahayobaid@gmail.com>
2025-11-12 08:44:30 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/Weather-App/',
-=======
->>>>>>> ed0bfca36daf20951b87a6a4a7fbff58a0fca2d4
 })
